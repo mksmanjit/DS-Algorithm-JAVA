1c5441a5bead1ca518c0a2686025a8661eacfb2f
Manjit Kumar <Manjit.Kumar@digitaleconomysolutions.com>
2022-07-19 09:54:48 +0400

    Floyd Warshall All Pair Shortest Path

---
+++ greedy/shortedPath/FloydWarshall.java
@@ -0,0 +1,62 @@
+package greedy.shortedPath;
+
+/**
+ * The Floyd Warshall Algorithm is for solving the All Pairs Shortest Path problem.
+ * The problem is to find shortest distances between every pair of vertices in a given edge weighted directed/undirected Graph.
+ *
+ * Time Complexity - O(V^3)
+ * Space Complexity - O(V^2)
+ *
+ * Shortest Path Using Dijkstra - O(ELogV)
+ * Finding All Pair Shortest Path Using Dijkstra - O(V*ELogV) = O(V^3LogV) , considering E = V^2
+ * Shortest Path Using Bellman Ford - O(EV) = O(V^3)
+ * Finding All Pair Shortest Path Using Bellman Ford - O(V*V^3) = O(V^4)
+ *
+ */
+public class FloydWarshall {
+    public static void main(String[] args) {
+//        long[][] weightedPathMatrix = {
+//                {Integer.MAX_VALUE, 1, Integer.MAX_VALUE, 5},
+//                {1, Integer.MAX_VALUE, 2, Integer.MAX_VALUE},
+//                {Integer.MAX_VALUE, 2, Integer.MAX_VALUE, 1},
+//                {5, Integer.MAX_VALUE, 1, Integer.MAX_VALUE},
+//        };
+        long[][] weightedPathMatrix = {
+                {Integer.MAX_VALUE, 11, 1, 6},
+                {11, Integer.MAX_VALUE, 7, 3},
+                {1, 7, Integer.MAX_VALUE, 2},
+                {6, 3, 2, Integer.MAX_VALUE},
+        };
+        long[][] allPairShortestPath = allPairShortestPath(weightedPathMatrix);
+        for (int i = 0; i < allPairShortestPath.length; i++) {
+            for (int j = 0; j < allPairShortestPath.length; j++) {
+                System.out.println("Path From " + i + " to " + j + " is " + allPairShortestPath[i][j]);
+            }
+        }
+    }
+
+    /**
+     * Total Time Complexity - O(V^3)
+     * Space Complexity - O(V^2), because of having extra memory for last path's.
+     *
+     * @param weightedPathMatrix1
+     * @return
+     */
+    public static long[][] allPairShortestPath(long[][] weightedPathMatrix1) {
+        long[][] weightedPathMatrix2 = new long[weightedPathMatrix1.length][weightedPathMatrix1.length];
+        for (int i = 0; i < weightedPathMatrix1.length; i++) { // O(V)
+            for (int j = 0; j < weightedPathMatrix1.length; j++) { // O(V)
+                for (int k = 0; k < weightedPathMatrix1.length; k++) { // O(V)
+                    if (j == k) continue;
+                    if (weightedPathMatrix1[j][k] > (weightedPathMatrix1[j][i] + weightedPathMatrix1[i][k])) { // O(1)
+                        weightedPathMatrix2[j][k] = weightedPathMatrix1[j][i] + weightedPathMatrix1[i][k];
+                    } else {
+                        weightedPathMatrix2[j][k] = weightedPathMatrix1[j][k]; // O(1)
+                    }
+                }
+            }
+            weightedPathMatrix1 = weightedPathMatrix2;
+        }
+        return weightedPathMatrix2;
+    }
+}
